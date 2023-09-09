4a2dcc9e431b24fee62eceafdca75f7fa5259bd6
Ambeeru Vignesh <vigneshvj53@gmail.com>
2023-09-09 11:39:02 +0530

    code correction

--- uniq (xv6 qemu version).c
+++ uniq (xv6 qemu version).c
@@ -9,43 +9,37 @@ int main(int argc, char *argv[])
         exit();
     }
 
-    int fd = open(argv[1], 0);
-    if (fd < 0)
+    char *filename = argv[1];
+    int file = open(filename, 0);
+
+    if (file < 0)
     {
-        printf(2, "Error: Cannot open file %s\n", argv[1]);
+        printf(2, "Error: Unable to open file %s\n", filename);
         exit();
     }
 
+    char currentChar;
     char curr_line[256];
     char prev_line[256] = "";
-    int first_line = 1;
+    int lineIndex = 0;
 
-    while (1)
+    while (read(file, &currentChar, 1) > 0)
     {
-        int n = read(fd, curr_line, sizeof(curr_line));
-        if (n < 0)
+        if (currentChar == '\n')
         {
-            printf(2, "Error reading file\n");
-            close(fd);
-            exit();
-        }
-        if (n == 0)
-            break;
-
-        curr_line[n] = '\0';
-
-        if (strcmp(curr_line, prev_line) != 0)
-        {
-            if (first_line)
+            curr_line[lineIndex] = '\0';
+            if (strcmp(curr_line, prev_line) != 0)
             {
-                first_line = 0;
-                printf(1, "Uniq command is getting executed in user mode.\n");
+                printf(1, "%s\n", curr_line);
+                strcpy(prev_line, curr_line);
             }
-            printf(1, "%s", curr_line);
-            strcpy(prev_line, curr_line);
+            lineIndex = 0;
+        }
+        else
+        {
+            curr_line[lineIndex++] = currentChar;
         }
     }
-
-    close(fd);
+    close(file);
     exit();
 }
